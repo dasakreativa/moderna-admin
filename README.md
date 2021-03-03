a49904932474f0ccc38cf13b52ba586f480817d3
dasakreativa <officialcakadi@gmail.com>
2021-03-03 22:14:01 +0700

    Versi Perdana 0.1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# Moderna Administrator Template
+![Moderna Administrator Temlate Logo](assets/img/brands/svg/logo-white.svg)
+
+Moderna admin adalah sebuah template administrator yang responsif dan elegan yang dibangun diatas kerangka kerja [Bootstrap 4.6](https://getbootstrap.com). Admin template ini dibawah perlindungan Lisensi [MIT](./LICENSE) dan kami harap anda membaca terlebih dahulu.
+
+# Kredit Gambar dan Pihak Ketiga
+1. [Unsplash](https://www.unsplash.com)
+1. [jQuery Validation](https://jqueryvalidations.com)
---
+++ assets/js/moderna-admin/all.js
@@ -0,0 +1,77 @@
+(function ($) {
+    "use strict";
+
+    $.clocker = ($target) => {
+        var months = [
+            'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+        ];
+
+        var days = [
+            'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+        ];
+
+        function update() {
+            var date = new Date();
+
+            var hours = date.getHours() < 10 ?
+                '0' + date.getHours() :
+                date.getHours();
+
+            var minutes = date.getMinutes() < 10 ?
+                '0' + date.getMinutes() :
+                date.getMinutes();
+
+            var seconds = date.getSeconds() < 10 ?
+                '0' + date.getSeconds() :
+                date.getSeconds();
+
+            var dayOfWeek = days[date.getDay()];
+            var month = months[date.getMonth()];
+            var day = date.getDate();
+            var year = date.getFullYear();
+
+            var dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year,
+                clock = hours + ' : ' + minutes + ' : ' + seconds;
+
+            $($target).text(clock);
+
+        }
+
+        update();
+        window.setInterval(update, 1000);
+    };
+
+    $.clocker('#clock');
+    $.clocker('#clockwrap');
+
+    $('.btn-toggler').click(() => {
+        $('body').toggleClass('toggled');
+    });
+
+    $(document).ready(function () {
+        $('.sidebar-menu').metisMenu({
+
+            // enabled/disable the auto collapse.
+            toggle: true,
+
+            // prevent default event
+            preventDefault: true,
+
+            // default classes
+            activeClass: 'active',
+            collapseClass: 'collapse',
+            collapseInClass: 'show',
+            collapsingClass: 'collapsing',
+
+            // .nav-link for Bootstrap 4
+            triggerElement: 'a',
+
+            // .nav-item for Bootstrap 4
+            parentTrigger: 'li',
+
+            // .nav.flex-column for Bootstrap 4
+            subMenu: 'ul'
+
+        });
+    })
+})(jQuery);
